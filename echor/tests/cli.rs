e36ed0282b5cfb6a9492a48a033095b8fa1cdf3f
himanushi <himanushi@users.noreply.github.com>
2024-03-27 08:27:22 +0900

    p.50

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -5,23 +5,6 @@ use std::fs;
 type TestResult = Result<(), Box<dyn std::error::Error>>;
 
 #[test]
-fn hello1() -> TestResult {
-    let expected = fs::read_to_string("tests/expected/hello1.txt")?;
-    let mut cmd = Command::cargo_bin("echor")?;
-    cmd.args(vec!["Hello", "there"])
-        .assert()
-        .success()
-        .stdout(expected);
-    Ok(())
-}
-
-#[test]
-fn runs() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.arg("hello").assert().success();
-}
-
-#[test]
 fn dies_no_args() -> TestResult {
     Command::cargo_bin("echor")?
         .assert()
@@ -29,4 +12,35 @@ fn dies_no_args() -> TestResult {
         .stderr(predicate::str::contains("USAGE"));
 
     Ok(())
+}
+
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin("echor")?
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected);
+
+    Ok(())
+}
+
+#[test]
+fn hello1() -> TestResult {
+    run(&["Hello there"], "tests/expected/hello1.txt")
+}
+
+#[test]
+fn hello2() -> TestResult {
+    run(&["Hello", "there"], "tests/expected/hello2.txt")
+}
+
+#[test]
+fn hello1_no_newline() -> TestResult {
+    run(&["Hello there", "-n"], "tests/expected/hello1.n.txt")
+}
+
+#[test]
+fn hello2_no_newline() -> TestResult {
+    run(&["-n", "Hello", "there"], "tests/expected/hello2.n.txt")
 }
