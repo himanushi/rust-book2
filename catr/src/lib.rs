86d21b91f67b00f4f95eb6b993ccc24bcaf959f1
himanushi <himanushi@users.noreply.github.com>
2024-03-29 09:17:05 +0900

    p.65

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,3 +1,4 @@
+use clap::{App, Arg};
 use std::error::Error;
 
 #[derive(Debug)]
@@ -9,7 +10,32 @@ pub struct Config {
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
-pub fn run() -> MyResult<()> {
-    println!("Hello, world!");
+pub fn run(config: Config) -> MyResult<()> {
+    dbg!(config);
     Ok(())
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("catr")
+        .version("0.1.0")
+        .author("himanushi")
+        .about("Rust cat")
+        .arg(
+            Arg::with_name("files")
+                .help("Files to read")
+                .default_value("-"),
+        )
+        .arg(Arg::with_name("number_lines").short("n").takes_value(false))
+        .arg(
+            Arg::with_name("number_nonblank_lines")
+                .short("b")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        number_lines: matches.is_present("number_lines"),
+        number_nonblank_lines: matches.is_present("number_nonblank_lines"),
+    })
 }
